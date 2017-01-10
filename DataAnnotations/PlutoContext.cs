f1a7e0c167850161da83d52b61330524cf331bab
Alexey Guryev <alguryew@gmail.com>
2017-01-10 08:39:16 +0300

    Data annotations

---
+++ DataAnnotations/PlutoContext.cs
@@ -0,0 +1,16 @@
+namespace DataAnnotations
+{
+    using System.Data.Entity;
+
+    public partial class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+    }
+}
