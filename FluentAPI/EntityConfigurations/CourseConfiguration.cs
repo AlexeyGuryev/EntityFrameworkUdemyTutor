f2747b13702cd5099b4a4a37a0bb8865fea8c5b2
Alexey Guryev <alguryew@gmail.com>
2017-01-10 12:33:39 +0300

    Fluent API

---
+++ FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            ToTable("Courses");
+            HasKey(c => c.Id);
+
+            Property(c => c.Name)
+               .IsRequired()
+               .HasMaxLength(255);
+
+            Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m =>
+                {
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+                    m.ToTable("CourseTags");
+                });
+        }
+    }
+}
---
+++ FluentAPI/PlutoContext.cs
@@ -0,0 +1,22 @@
+namespace FluentAPI
+{
+    using EntityConfigurations;
+    using System.Data.Entity;
+
+    public partial class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CourseConfiguration());
+        }
+    }
+}
